b34f34a5144a7a213c2eadb8e7c07527b4992aac
Mark Bradley <mark.bradley@sainsburys.co.uk>
2017-03-15 12:19:57 +0000

    Starting Templates
    Creating the Integer and Integer test classes ready for the start of an assessment.

---
+++ src/Integer.php
@@ -0,0 +1,3 @@
+<?php
+namespace Braddle\Integer;
+
---
+++ test/IntegerTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Braddle\Integer\Test;
+
+use PHPUnit\Framework\TestCase;
+use Braddle\Integer\Integer;
+
+class IntegerTest extends TestCase
+{
+
+}
